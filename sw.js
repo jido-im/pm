b919bad0e423655f7be4826925cf92700f8f9771
jido.im <jisan@jido.im>
2026-08-19 11:44:45 +0900

    Update jido app icon and favicon

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='jido-pm-shell-v1';
-const SHELL=['./','./index.html','./manifest.webmanifest','./app-icon.svg'];
+const CACHE='jido-pm-shell-v2';
+const SHELL=['./','./index.html','./manifest.webmanifest','./app-icon-192.png','./app-icon-512.png'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL))));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key))))));
 self.addEventListener('fetch',event=>{
